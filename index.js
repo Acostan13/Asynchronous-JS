146c04f0842ba44ddfbb5409599199d12772f0bf
Alex Costan <acostan.con@orangetheory.com>
2020-09-23 10:53:36 -0400

    returning values from async functions

--- index.js
+++ index.js
@@ -19,37 +19,63 @@ const writeFilePro = (file, data) => {
     })
 }
 
-readFilePro(`${__dirname}/dog.txt`)
-    .then((data) => {
-        console.log(`Breed: ${data}`)
+// readFilePro(`${__dirname}/dog.txt`)
+//     .then((data) => {
+//         console.log(`Breed: ${data}`)
 
-        return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
-    })
-    .then((res) => {
-        console.log(res.body.message)
+//         return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+//     })
+//     .then((res) => {
+//         console.log(res.body.message)
 
-        return writeFilePro('dog-img.txt', res.body.message)
-    })
-    .then(() => {
-        console.log('Random dog image saved to file!')
-    })
-    .catch((err) => {
-        console.log(err.message)
-    })
+//         return writeFilePro('dog-img.txt', res.body.message)
+//     })
+//     .then(() => {
+//         console.log('Random dog image saved to file!')
+//     })
+//     .catch((err) => {
+//         console.log(err.message)
+//     })
 
 // async functions will work on code while not affecting the event loop
+// returns a promise
 const getDogPic = async () => {
     try {
         const data = await readFilePro(`${__dirname}/dog.txt`)
         console.log(`Breed: ${data}`)
-    
+
         const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
         console.log(res.body.message)
-    
+
         await writeFilePro('dog-img.txt', res.body.message)
         console.log('Random dog image saved to file!')
     } catch (err) {
         console.log(err)
+        throw err
     }
+    return '2: READY 🐶'
 }
+
+// IFEE async await
+(async () => {
+    try {
+        console.log('1: Will get dog pics!')
+        const x = await getDogPic()
+        console.log(x)
+        console.log('3: Done getting dog pics!')
+    } catch(err) {
+        console.log('ERROR 💥', err)
+    }
+})()
+
+/*
+console.log('1: Will get dog pics!')
 getDogPic()
+    .then((x) => {
+        console.log(x)
+        console.log('3: Done getting dog pics!')
+    })
+    .catch((err) => {
+        console.log('ERROR 💥')
+    })
+*/
